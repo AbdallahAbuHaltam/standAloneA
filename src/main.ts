c8fa05dacdd15e301c12254def9500af2707b71b
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-11-23 15:53:41 +0300

    Added

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
+import { AnalyticsService } from "./app/shared/analytics.service";
 
-import { AppModule } from './app/app.module';
+bootstrapApplication(AppComponent,{
+    providers:[AnalyticsService]
+});
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
